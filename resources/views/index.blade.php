1f124a7805dd6fa0f973c15107fa4c7a5386459a
geethanchanaka <chanakageethan@gmail.com>
2025-09-12 14:51:20 +0530

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+
+
+@extends('layouts.app')
+
+@section('title','List of tasks')
+
+
+@section('content')
+
+
+
+
+
+
+
+<div>
+<!-- @if(count($tasks)) -->
+@forelse($tasks as $task)
+ <div>
+    <a href="{{route('tasks.show',['id'=> $task->id])}}">{{$task->title}}</a>
+</div>
+
+@empty
+ <div>There are no tasks!</div>
+@endforelse 
+<!-- @endif -->
+
+</div>
+
+
+@endsection('content')
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+
+<head>
+    <title>Laravel 10 Task List App</title>
+</head>
+
+
+<body>
+    <h1>@yield('title')</h1>
+
+<div>
+@yield('content')
+</div>
+
+
+</body>
+</html>
