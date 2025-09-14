f8980790309b9acdeeb7ddd80b79d2c1979dbe4f
geethanchanaka <chanakageethan@gmail.com>
2025-09-14 15:37:33 +0530

    insert data

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
+use App\Models\Task;
 
 
 
@@ -23,12 +23,31 @@ Route::view('/tasks/create','create')->name('tasks.create');
 
 
 Route::get('/tasks/{id}',function($id) {
-   return view('show',['task'=> \App\Models\Task::findOrFail($id)]);
+   return view('show',['task'=> Task::findOrFail($id)]);
 })->name('tasks.show');
 
 
 Route::post('/tasks',function (Request $request){
-    dd($request->all());
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+  ]);
+
+
+  $task = new Task;
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  
+
+  $task->save();
+
+  return redirect()->route('tasks.show',['id' =>$task->id]);
+  
+
+
+
 })->name('tasks.store');
 
 
